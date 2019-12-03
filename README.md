3099ad8860dfbedd90adb151d6f2a6b62ae553ce
Jarrod Kallis <jarrod.kallis@altron.com>
2019-12-03 13:31:06 +0200

    JDBC

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# jdbc-jpa-database-demo 
+# jdbc-jpa-database-demo
+
+[https://github.com/in28minutes/jpa-with-hibernate](https://github.com/in28minutes/jpa-with-hibernate) 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- This gets called because of auto configuration, but it must be called data.sql
+create table person (
+	id integer not null,
+	name varchar(255) not null,
+	location varchar(255),
+	dob timestamp not null,
+	primary key (id)
+);
+
+insert into person values (10001, 'Jarrod', 'Home', sysdate() - 3000);
+insert into person values (10002, 'Tess', 'Work', sysdate() - 2000);
+insert into person values (10003, 'Ben', 'School', sysdate() - 300);
